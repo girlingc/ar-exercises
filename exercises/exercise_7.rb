3027688b02abdb3b372a433bb28c2ebd498afb52
Christian Girling <girling.christian@gmail.com>
2022-03-13 17:25:20 -0700

    Completed exercise_7

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,9 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+
+puts "Hello, User. What is the the store name?"
+answer = gets.chomp
+store = Store.create(name: answer, mens_apparel: true)
+
+puts store.errors.full_messages
